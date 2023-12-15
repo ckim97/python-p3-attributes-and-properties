7d0ee135e3c22a6810dd389d5dceb26749db51b8
Christopher <chrisjwkim97@gmail.com>
2023-12-14 21:38:06 -0500

    broken lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+
+fido = Dog("Fido", "Corgi")
+print(fido.name)
+print(fido.breed)
